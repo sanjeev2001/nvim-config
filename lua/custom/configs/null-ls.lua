01e62fb9265aa7d9ec26e678753d798fc0cab57a
Sanjeev Sritharan <sanjeev.sritharan007@gmail.com>
2024-04-15 02:26:08 -0400

    added python debugger

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -3,7 +3,12 @@ local null_ls = require('null-ls')
 
 local opts = {
   sources = {
-    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy.with({
+      extra_args = function()
+      local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+      return { "--python-executable", virtual .. "/bin/python3" }
+      end,
+    }),
     require("none-ls.diagnostics.ruff"),
     null_ls.builtins.formatting.black,
   },
